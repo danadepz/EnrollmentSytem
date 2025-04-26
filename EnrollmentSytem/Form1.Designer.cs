b88ae5443574d6384db63aa4e52ce35c4a3de960
kimpc <kimpc@DAWNNN>
2025-04-26 10:02:16 +0800

    Add project files.

---
+++ EnrollmentSytem/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace EnrollmentSytem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IdNumberLabel = new System.Windows.Forms.Label();
+            this.FirstNameLabel = new System.Windows.Forms.Label();
+            this.MiddleInitialLabel = new System.Windows.Forms.Label();
+            this.LastNameLabel = new System.Windows.Forms.Label();
+            this.CourseLabel = new System.Windows.Forms.Label();
+            this.YearLabel = new System.Windows.Forms.Label();
+            this.RemarksLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.IdNumberTextBox = new System.Windows.Forms.TextBox();
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.MiddleInitialTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.YearTextBox = new System.Windows.Forms.TextBox();
+            this.CourseTextBox = new System.Windows.Forms.TextBox();
+            this.RemarksTextBox = new System.Windows.Forms.ComboBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.StudentEntryLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // IdNumberLabel
+            // 
+            this.IdNumberLabel.AutoSize = true;
+            this.IdNumberLabel.Location = new System.Drawing.Point(117, 92);
+            this.IdNumberLabel.Name = "IdNumberLabel";
+            this.IdNumberLabel.Size = new System.Drawing.Size(58, 13);
+            this.IdNumberLabel.TabIndex = 0;
+            this.IdNumberLabel.Text = "ID Number";
+            // 
+            // FirstNameLabel
+            // 
+            this.FirstNameLabel.AutoSize = true;
+            this.FirstNameLabel.Location = new System.Drawing.Point(117, 120);
+            this.FirstNameLabel.Name = "FirstNameLabel";
+            this.FirstNameLabel.Size = new System.Drawing.Size(52, 13);
+            this.FirstNameLabel.TabIndex = 1;
+            this.FirstNameLabel.Text = "Firstname";
+            // 
+            // MiddleInitialLabel
+            // 
+            this.MiddleInitialLabel.AutoSize = true;
+            this.MiddleInitialLabel.Location = new System.Drawing.Point(117, 146);
+            this.MiddleInitialLabel.Name = "MiddleInitialLabel";
+            this.MiddleInitialLabel.Size = new System.Drawing.Size(65, 13);
+            this.MiddleInitialLabel.TabIndex = 2;
+            this.MiddleInitialLabel.Text = "Middle Initial";
+            // 
+            // LastNameLabel
+            // 
+            this.LastNameLabel.AutoSize = true;
+            this.LastNameLabel.Location = new System.Drawing.Point(117, 174);
+            this.LastNameLabel.Name = "LastNameLabel";
+            this.LastNameLabel.Size = new System.Drawing.Size(53, 13);
+            this.LastNameLabel.TabIndex = 3;
+            this.LastNameLabel.Text = "Lastname";
+            // 
+            // CourseLabel
+            // 
+            this.CourseLabel.AutoSize = true;
+            this.CourseLabel.Location = new System.Drawing.Point(117, 203);
+            this.CourseLabel.Name = "CourseLabel";
+            this.CourseLabel.Size = new System.Drawing.Size(40, 13);
+            this.CourseLabel.TabIndex = 4;
+            this.CourseLabel.Text = "Course";
+            // 
+            // YearLabel
+            // 
+            this.YearLabel.AutoSize = true;
+            this.YearLabel.Location = new System.Drawing.Point(117, 231);
+            this.YearLabel.Name = "YearLabel";
+            this.YearLabel.Size = new System.Drawing.Size(29, 13);
+            this.YearLabel.TabIndex = 5;
+            this.YearLabel.Text = "Year";
+            // 
+            // RemarksLabel
+            // 
+            this.RemarksLabel.AutoSize = true;
+            this.RemarksLabel.Location = new System.Drawing.Point(117, 261);
+            this.RemarksLabel.Name = "RemarksLabel";
+            this.RemarksLabel.Size = new System.Drawing.Size(49, 13);
+            this.RemarksLabel.TabIndex = 6;
+            this.RemarksLabel.Text = "Remarks";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(368, 335);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 7;
+            // 
+            // IdNumberTextBox
+            // 
+            this.IdNumberTextBox.Location = new System.Drawing.Point(197, 85);
+            this.IdNumberTextBox.Name = "IdNumberTextBox";
+            this.IdNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.IdNumberTextBox.TabIndex = 8;
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.Location = new System.Drawing.Point(197, 113);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(156, 20);
+            this.FirstNameTextBox.TabIndex = 9;
+            // 
+            // MiddleInitialTextBox
+            // 
+            this.MiddleInitialTextBox.Location = new System.Drawing.Point(197, 143);
+            this.MiddleInitialTextBox.Name = "MiddleInitialTextBox";
+            this.MiddleInitialTextBox.Size = new System.Drawing.Size(156, 20);
+            this.MiddleInitialTextBox.TabIndex = 10;
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Location = new System.Drawing.Point(197, 174);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(156, 20);
+            this.LastNameTextBox.TabIndex = 11;
+            // 
+            // YearTextBox
+            // 
+            this.YearTextBox.Location = new System.Drawing.Point(197, 231);
+            this.YearTextBox.Name = "YearTextBox";
+            this.YearTextBox.Size = new System.Drawing.Size(100, 20);
+            this.YearTextBox.TabIndex = 12;
+            // 
+            // CourseTextBox
+            // 
+            this.CourseTextBox.Location = new System.Drawing.Point(197, 203);
+            this.CourseTextBox.Name = "CourseTextBox";
+            this.CourseTextBox.Size = new System.Drawing.Size(156, 20);
+            this.CourseTextBox.TabIndex = 13;
+            // 
+            // RemarksTextBox
+            // 
+            this.RemarksTextBox.FormattingEnabled = true;
+            this.RemarksTextBox.Location = new System.Drawing.Point(197, 261);
+            this.RemarksTextBox.Name = "RemarksTextBox";
+            this.RemarksTextBox.Size = new System.Drawing.Size(100, 21);
+            this.RemarksTextBox.TabIndex = 14;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(159, 299);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 15;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(240, 299);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 16;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // StudentEntryLabel
+            // 
+            this.StudentEntryLabel.AutoSize = true;
+            this.StudentEntryLabel.Location = new System.Drawing.Point(194, 38);
+            this.StudentEntryLabel.Name = "StudentEntryLabel";
+            this.StudentEntryLabel.Size = new System.Drawing.Size(99, 13);
+            this.StudentEntryLabel.TabIndex = 17;
+            this.StudentEntryLabel.Text = "STUDENT ENTRY";
+            this.StudentEntryLabel.Click += new System.EventHandler(this.StudentEntryLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 450);
+            this.Controls.Add(this.StudentEntryLabel);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.RemarksTextBox);
+            this.Controls.Add(this.CourseTextBox);
+            this.Controls.Add(this.YearTextBox);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.MiddleInitialTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Controls.Add(this.IdNumberTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.RemarksLabel);
+            this.Controls.Add(this.YearLabel);
+            this.Controls.Add(this.CourseLabel);
+            this.Controls.Add(this.LastNameLabel);
+            this.Controls.Add(this.MiddleInitialLabel);
+            this.Controls.Add(this.FirstNameLabel);
+            this.Controls.Add(this.IdNumberLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label IdNumberLabel;
+        private System.Windows.Forms.Label FirstNameLabel;
+        private System.Windows.Forms.Label MiddleInitialLabel;
+        private System.Windows.Forms.Label LastNameLabel;
+        private System.Windows.Forms.Label CourseLabel;
+        private System.Windows.Forms.Label YearLabel;
+        private System.Windows.Forms.Label RemarksLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox IdNumberTextBox;
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox MiddleInitialTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.TextBox YearTextBox;
+        private System.Windows.Forms.TextBox CourseTextBox;
+        private System.Windows.Forms.ComboBox RemarksTextBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label StudentEntryLabel;
+    }
+}
+
---
+++ EnrollmentSytem/Form3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+string connStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\kimpc\Desktop\dePaz_EnrollmentSystem\EnrollentSystem.accdb;";
+try
+{
+    using (OleDbConnection conn = new OleDbConnection(connStr))
+    {
+        conn.Open();
+        MessageBox.Show("Connection successful! 🎉");
+    }
+}
+catch (Exception ex)
+{
+    MessageBox.Show($"Error: {ex.Message}");
+}
